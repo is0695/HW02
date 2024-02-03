47e1610bf0a3adf2683eeb0b72266ae37b0eb513
is0695 <123656972+is0695@users.noreply.github.com>
2024-02-03 22:12:47 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -74,15 +74,10 @@ def main():
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
 
-    # TODO 1: выполните пункты 1-3 ДЗ
-
     contacts_list = fix_phonebook(contacts_list)
-
-    # TODO 2: сохраните получившиеся данные в другой файл
-    # код для записи файла в формате CSV
+    
     with open(r"phonebook.csv", "w",  encoding="utf-8", newline="") as f:
         datawriter = csv.writer(f, delimiter=',')
-    #   # Вместо contacts_list подставьте свой список
         datawriter.writerows(contacts_list)
 
 
